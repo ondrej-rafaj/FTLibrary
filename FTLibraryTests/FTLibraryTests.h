c6af13409aba1d11a06481cf3782fcda158f0dde
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2011-06-28 13:59:52 +0100

    first upload

---
+++ FTLibraryTests/FTLibraryTests.h
@@ -0,0 +1,17 @@
+//
+//  FTLibraryTests.h
+//  FTLibraryTests
+//
+//  Created by Ondrej Rafaj on 28/06/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface FTLibraryTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ FTLibraryTests/FTLibraryTests.m
@@ -0,0 +1,33 @@
+//
+//  FTLibraryTests.m
+//  FTLibraryTests
+//
+//  Created by Ondrej Rafaj on 28/06/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import "FTLibraryTests.h"
+
+
+@implementation FTLibraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FTLibraryTests");
+}
+
+@end
