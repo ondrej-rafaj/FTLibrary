97a4f07bd6ec7d6bb20b16598ab91fc83285ccf8
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2011-06-29 14:18:40 +0100

    adding killswitch class + reordering

---
+++ FTLibrary/FTSystemKillSwitch.h
@@ -0,0 +1,17 @@
+//
+//  FTSystemKillSwitch.h
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 29/06/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FTSystemKillSwitch : NSObject
+
++ (BOOL)isAppEnabled;
+
+
+@end
---
+++ FTLibrary/FTSystemKillSwitch.m
@@ -0,0 +1,15 @@
+//
+//  FTSystemKillSwitch.m
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 29/06/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import "FTSystemKillSwitch.h"
+#import "FTDataJson.h"
+
+
+@implementation FTSystemKillSwitch
+
+@end
