a138b1b41d4adabfff97d65bc35fed1ec179e506
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2011-08-15 10:37:48 +0100

    adding aqgridview
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ FTLibrary/FTAutoGridView.h
@@ -0,0 +1,13 @@
+//
+//  FTAutoGridView.h
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 14/08/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import "FTView.h"
+
+@interface FTAutoGridView : FTView
+
+@end
