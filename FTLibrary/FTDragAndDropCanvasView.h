94b6c5d7fda53fc4797c4a2aafdac46adbb286a5
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2011-08-16 14:55:03 +0100

    Drag&Drop canvas
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

---
+++ FTLibrary/FTDragAndDropCanvasView.h
@@ -0,0 +1,60 @@
+//
+//  FTDragAndDropCanvasView.h
+//  DDrop
+//
+//  Created by Ondrej Rafaj on 03/04/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FTDragAndDropView.h"
+
+
+typedef enum {
+    
+    FTDragAndDropCanvasViewStatusMoving,
+    FTDragAndDropCanvasViewStatusRotating
+    
+} FTDragAndDropCanvasViewStatus;
+
+
+@class FTDragAndDropCanvasView;
+
+@protocol FTDragAndDropCanvasViewDelegate <NSObject>
+
+- (void)finishedEditingElement:(FTDragAndDropView *)element withData:(NSDictionary *)data;
+
+- (void)deletedElement:(FTDragAndDropView *)element withIndex:(int)index;
+
+- (void)createdElement:(FTDragAndDropView *)element withData:(NSDictionary *)data;
+
+@end
+
+
+@interface FTDragAndDropCanvasView : UIView <UIGestureRecognizerDelegate> {
+    
+    NSMutableArray *elements;
+    
+    UIImageView *backgroundImageView;
+    
+    FTDragAndDropView *activeElement;
+    
+    id <FTDragAndDropCanvasViewDelegate> delegate;
+    
+}
+
+@property (nonatomic, retain) UIImageView *backgroundImageView;
+
+@property (nonatomic, assign) id <FTDragAndDropCanvasViewDelegate> delegate;
+
+
+- (void)setBackgroundImage:(UIImage *)backgroundImage;
+
+- (void)addElementFromPath:(NSString *)imagePath;
+
+- (void)addElementFromData:(NSDictionary *)data;
+
+- (void)layoutElements:(BOOL)animated;
+
+
+@end
---
+++ FTLibrary/FTDragAndDropView.h
@@ -0,0 +1,57 @@
+//
+//  FTDragAndDropView.h
+//  DDrop
+//
+//  Created by Ondrej Rafaj on 03/04/2011.
+//  Copyright 2011 Fuerte International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FTDragAndDropView : UIView {
+    
+    UIImageView *imageView;
+    
+    CGFloat lastRotation;
+    CGFloat lastScale;
+    
+    NSString *imagePath;
+    
+    NSDictionary *elementData;
+    
+    CGFloat positionX;
+	CGFloat positionY;
+    
+    // Real return values
+    CGFloat realRotationValue;
+    CGFloat realScaleValue;
+    
+}
+
+@property (nonatomic, retain) UIImageView *imageView;
+
+@property (nonatomic) CGFloat lastRotation;
+@property (nonatomic) CGFloat lastScale;
+
+@property (nonatomic, retain) NSString *imagePath;
+
+@property (nonatomic, retain) NSDictionary *elementData;
+
+@property (nonatomic) CGFloat positionX;
+@property (nonatomic) CGFloat positionY;
+
+@property (nonatomic) CGFloat realRotationValue;
+@property (nonatomic) CGFloat realScaleValue;
+
+
+- (id)initWithImagePath:(NSString *)path;
+
+- (id)initWithImageData:(NSDictionary *)data;
+
+- (void)setPosition:(CGPoint)position;
+
+- (NSDictionary *)getInfo;
+
+
+@end
