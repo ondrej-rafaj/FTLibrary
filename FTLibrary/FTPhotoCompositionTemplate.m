6a2424c506d08cd431770bfab618878763fdd524
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2011-10-25 23:37:59 +0100

    Adding Photo Book views

---
+++ FTLibrary/FTPhotoCompositionTemplate.m
@@ -0,0 +1,13 @@
+//
+//  FTPhotoCompositionTemplate.m
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 25/10/2011.
+//  Copyright (c) 2011 Fuerte International. All rights reserved.
+//
+
+#import "FTPhotoCompositionTemplate.h"
+
+@implementation FTPhotoCompositionTemplate
+
+@end
---
+++ FTLibrary/FTPhotoHoldingView.m
@@ -0,0 +1,13 @@
+//
+//  FTPhotoHoldingView.m
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 25/10/2011.
+//  Copyright (c) 2011 Fuerte International. All rights reserved.
+//
+
+#import "FTPhotoHoldingView.h"
+
+@implementation FTPhotoHoldingView
+
+@end
