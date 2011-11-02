4363944452d8780075a62aeff853607541ef83cb
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2011-11-02 10:58:21 +0000

    adding scrollable clock class

---
+++ FTLibrary/FTScrollableClockView.h
@@ -0,0 +1,13 @@
+//
+//  FTScrollableClockView.h
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 02/11/2011.
+//  Copyright (c) 2011 Fuerte International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTScrollableClockView : UIView
+
+@end
---
+++ FTLibrary/FTScrollableClockView.m
@@ -0,0 +1,31 @@
+//
+//  FTScrollableClockView.m
+//  FTLibrary
+//
+//  Created by Ondrej Rafaj on 02/11/2011.
+//  Copyright (c) 2011 Fuerte International. All rights reserved.
+//
+
+#import "FTScrollableClockView.h"
+
+@implementation FTScrollableClockView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
