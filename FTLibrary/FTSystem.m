1eea1f406d2adfc4958d6d1c41e597988497a246
Andy Martin <andymartinwork@gmail.com>
2012-06-14 10:55:14 +0100

    Fixed font listing method to show family name as well as font

--- FTLibrary/FTSystem.m
+++ FTLibrary/FTSystem.m
@@ -154,7 +154,7 @@ static float systemVersion = -1;
 	for (NSString *fontFamilyName in [UIFont familyNames]) {
 		[string appendFormat:@"\n%@", fontFamilyName];
 		for (NSString *fontName in [UIFont fontNamesForFamilyName:fontFamilyName]) {
-			[string appendFormat:@"\n\t%@", fontFamilyName];
+			[string appendFormat:@"\n\t%@", fontName];
 		}
 	}
 	NSLog(@"AVailable Fonts:%@", string);
